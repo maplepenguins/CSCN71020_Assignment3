44062414dd391b9bf851e37b8146b60455df2131
maplepenguins <thomsonmi11@gmail.com>
2024-10-31 09:10:00 -0400

    Add project files.

---
+++ RockPaperScissorUnitTest/RockPaperScissorUnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RockPaperScissorUnitTest
+{
+	TEST_CLASS(RockPaperScissorUnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
