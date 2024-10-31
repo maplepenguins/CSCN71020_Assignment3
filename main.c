4a2524a8525194a06c23b437af7b2b1c07996906
maplepenguins <thomsonmi11@gmail.com>
2024-10-31 14:19:46 -0400

    define valid moves of function, and checks to see if moves are valid

--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 #include "RockPaperScissor.h"
 
 char* RockPaperScissor(char player1[], char player2[]) {
+	//Defines what values are valid
+	const char* ValidMoves[] = { "Rock", "Paper", "Scissor" };
+
+
+	if (strlen(player1) == 0 || strlen(player2) == 0) { //checks for empty strings
+		return "Invalid";
+	}
+
+	int valid1 = 0;
+	int valid2 = 0;
+	for (int i = 0; i < 3; i++) { // checks to see if player1 and player 2 moves are valid
+		if (strcmp(player1, ValidMoves[i]) == 0) {
+			valid1 = 1;
+		}
+
+		if (strcmp(player2, ValidMoves[i]) == 0) {
+			valid2 = 1;
+		}
+	}
+
+	if (!valid1 || !valid2) { // if either input was invalid, it will return invalid
+		return "Invalid";
+	}
 
 
 }
